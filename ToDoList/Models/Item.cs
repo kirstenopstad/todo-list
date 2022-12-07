1cb62460c7e7c2d44624575ccb4d65ebe3bf3825
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-12-07 10:24:14 -0800

    passes test GetListNumber_ReturnsItemListNumber_ItemListNumber()
    Co-authored-by: Daniel Yakovlev <daniel.yakovlev@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -2,15 +2,18 @@ using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
+
   public class Item
   {
     public string Description { get; set; }
+    public int ListNumber { get; set; }
     private static List<Item> _instances = new List<Item>{};
 
     public Item(string description)
     {
       Description = description;
       _instances.Add(this);
+      ListNumber = _instances.Count;
     }
 
     public static List<Item> GetAll()
